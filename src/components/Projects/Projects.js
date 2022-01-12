efb35484c5d5547c3508be8468222e78c9bb4f2c
shamsrza <66381642+shamsrza@users.noreply.github.com>
2022-01-12 22:49:27 +0100

    delete project description

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,44 +1,63 @@
-import React from 'react';
+import React from "react";
 
-import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { projects } from '../../constants/constants';
+import {
+  BlogCard,
+  CardInfo,
+  ExternalLinks,
+  GridContainer,
+  HeaderThree,
+  Hr,
+  Tag,
+  TagList,
+  TitleContent,
+  UtilityList,
+  Img
+} from "./ProjectsStyles";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle
+} from "../../styles/GlobalComponents";
+import { projects } from "../../constants/constants";
 
 const Projects = () => (
   <Section nopadding id="projects">
-    
-    <SectionTitle main >
-    <SectionDivider style={{marginBottom: "20px"}}/>
+    <SectionTitle main>
+      <SectionDivider style={{ marginBottom: "20px" }} />
       Projects
     </SectionTitle>
-    <SectionText>
-    Showcase of my best work in a variety of fields.
-    </SectionText>
+    <SectionText>Showcase of my best work in a variety of fields.</SectionText>
     <GridContainer>
-      {projects.map(({id,image,title, description, tags, source, visit}) => (
-        <BlogCard key={id}>
-          <Img src={image}/>
-          <TitleContent>
-            <HeaderThree title>{title}</HeaderThree>
-            <Hr/>
-          </TitleContent>
-          <CardInfo>{description}</CardInfo>
-          <div>
-            <TitleContent>Stack</TitleContent>
-            <TagList>
-              {tags.map((tag, i) => (
-                <Tag key={i}>{tag}</Tag>
-              ))}
-            </TagList>
-          </div>
-          <UtilityList>
-            <ExternalLinks href={source} target="_blank">Code</ExternalLinks>
-            <ExternalLinks href={visit} target="_blank">Website</ExternalLinks>
-          </UtilityList>
-        </BlogCard>
-      ))}
+      {projects.map(
+        ({ id, image, title, description, tags, source, visit }) => (
+          <BlogCard key={id}>
+            <Img src={image} />
+            <TitleContent>
+              <HeaderThree title>{title}</HeaderThree>
+              {/* <Hr /> */}
+            </TitleContent>
+            {/* <CardInfo>{description}</CardInfo> */}
+            <div>
+              {/* <TitleContent>Stack</TitleContent> */}
+              <TagList>
+                {tags.map((tag, i) => (
+                  <Tag key={i}>{tag}</Tag>
+                ))}
+              </TagList>
+            </div>
+            <UtilityList>
+              <ExternalLinks href={source} target="_blank">
+                Code
+              </ExternalLinks>
+              <ExternalLinks href={visit} target="_blank">
+                Website
+              </ExternalLinks>
+            </UtilityList>
+          </BlogCard>
+        )
+      )}
     </GridContainer>
-
   </Section>
 );
 
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,46 +1,49 @@
 export const projects = [
   {
-    title: 'Medical Services Chat',
-    description: "Realtime Chat Messaging App with Authentication & SMS Notifications, Direct and Group Chats, Emojis & Reactions, Built-in Gif support, the ability to edit and delete messages, specialized commands, and much more, this fully responsive Medical Pager. ",
-      image: '/images/coming_soon.jpg',
-      tags: ['JS', 'Stream', 'React', 'Node'],
-    source: '/images/coming_soon.png',
-    visit: '/images/coming_soon.png',
-    id: 0,
+    title: "Medical Services Chat",
+    // description: "Realtime Chat Messaging App with Authentication & SMS Notifications, Direct and Group Chats, Emojis & Reactions, Built-in Gif support, the ability to edit and delete messages, specialized commands, and much more, this fully responsive Medical Pager. ",
+    image: "/images/coming_soon.jpg",
+    tags: ["JS", "Stream", "React", "Node"],
+    source: "/images/coming_soon.png",
+    visit: "/images/coming_soon.png",
+    id: 0
   },
   {
-    title: 'Cryptoverse Web Application',
-    description:"Cryptoverse App covers in-depth data about all crypto currencies, crypto markets and exchanges. Also application presents popular cryptocurrency news, charts (using Chart.js), data from multiple sources using RapidAPI.",
-    image: '/images/crypto.png',
-    tags: ['Redux', 'RapidAPI', 'Chart.js'],
-    source: 'https://github.com/shamsrza/crypto-app',
-    visit: 'https://crypto-reactapp.netlify.app',
-    id: 1,
+    title: "Cryptoverse Web Application",
+    //description:"Cryptoverse App covers in-depth data about all crypto currencies, crypto markets and exchanges. Also application presents popular cryptocurrency news, charts (using Chart.js), data from multiple sources using RapidAPI.",
+    image: "/images/crypto.png",
+    tags: ["Redux", "RapidAPI", "Chart.js"],
+    source: "https://github.com/shamsrza/crypto-app",
+    visit: "https://crypto-reactapp.netlify.app",
+    id: 1
   },
   {
-    title: 'eCommerce Web Application',
-    description: "This is online shopping website where customer will be able to add items to shopping cart, pay using PayPal and manage orders. Also admin panel with CRUD funcionalities will allow admin to create, update, delete products and categories.",
-      image: '/images/coming_soon.jpg',
-      tags: ['JS', "ASP.NET MVC", 'SQL'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 2,
+    title: "eCommerce Web Application",
+    //description: "This is online shopping website where customer will be able to add items to shopping cart, pay using PayPal and manage orders. Also admin panel with CRUD funcionalities will allow admin to create, update, delete products and categories.",
+    image: "/images/coming_soon.jpg",
+    tags: ["JS", "ASP.NET MVC", "SQL"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 2
   },
   {
-    title: 'Taste Restaurant Web Application',
-    description: "Taste is restaurant web application about menu, prices and services. App also presents two main CRUD functionalities: after signing in customer can make an order and also book a table, also can manage  all orders and reservations. ",
-    image: '/images/coming_soon.jpg',
-    tags: ['React', 'MaterialUI', 'WebAPI'],
-    source: 'https://github.com/shamsrza/TasteRestaurant',
-    visit: '/images/coming_soon.jpg',
-    id: 3,
-  },
+    title: "Taste Restaurant Web Application",
+    //description: "Taste is restaurant web application about menu, prices and services. App also presents two main CRUD functionalities: after signing in customer can make an order and also book a table, also can manage  all orders and reservations. ",
+    image: "/images/coming_soon.jpg",
+    tags: ["React", "MaterialUI", "WebAPI"],
+    source: "https://github.com/shamsrza/TasteRestaurant",
+    visit: "/images/coming_soon.jpg",
+    id: 3
+  }
 ];
 
 export const TimeLineData = [
-  { date: 2020.04, text: 'Started my journey.', },
-  { date: 2020.06, text: 'First projects as self-taught.', },
-  { date: 2020.11, text: 'Enrolled on Code Academy and got mentors.', },
-  { date: 2021.03, text: 'Created bunch of small and big projects with professional support.', },
-  { date: 2021.09, text: 'Graduated Academy and continue the "battle". ', },
+  { date: 2020.04, text: "Started my journey." },
+  { date: 2020.06, text: "First projects as self-taught." },
+  { date: 2020.11, text: "Enrolled on Code Academy and got mentors." },
+  {
+    date: 2021.03,
+    text: "Created bunch of small and big projects with professional support."
+  },
+  { date: 2021.09, text: 'Graduated Academy and continue the "battle". ' }
 ];
